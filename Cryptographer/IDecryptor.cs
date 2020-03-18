1f1bea4229f2d684726ad04f8e00591415d53055
Xpresence <alezzigrriff@gmail.com>
2020-03-18 18:31:30 +0300

    Added interfaces for encryption

---
+++ Cryptographer/IDecryptor.cs
@@ -0,0 +1,13 @@
+﻿namespace Cryptographer
+{
+    interface IDecryptor
+    {
+        //T DecryptKey<T>(string key);
+        T DecryptAES<T>(string key, string key_AES, string IV_AES);
+        bool VerifyHashRSA(string hash, string publicKey);
+
+        string PublicKey_RSA { get; }
+        string Key_AES { get; }
+        string IV_AES { get; }
+    }
+}
---
+++ Cryptographer/IEncryptor.cs
@@ -0,0 +1,13 @@
+﻿namespace Cryptographer
+{
+    interface IEncryptor
+    {
+        string EncryptKey<T>(T data);
+        string EncryptHashRSA<T>(T data, string privateKey);
+        string EncryptAES<T>(T data, string key, string IV);
+
+        string PrivateKey_RSA { get; }
+        string Key_AES { get; }
+        string IV_AES { get; }
+    }
+}
